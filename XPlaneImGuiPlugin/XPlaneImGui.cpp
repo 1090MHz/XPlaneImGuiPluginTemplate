f7e3b873c90794bf831d20e79865808b89693c39
Alan Duffy <alan.duffy@usask.ca>
2024-07-22 20:03:02 -0600

    remove flight_loop_callback (not intended for drawing)

--- XPlaneImGuiPlugin/XPlaneImGui.cpp
+++ XPlaneImGuiPlugin/XPlaneImGui.cpp
@@ -3,16 +3,6 @@
 #include "XPLMGraphics.h"
 #include "imgui_impl_xplane.h"
 
-float flight_loop_callback(float elapsed_since_last_call, float elapsed_since_last_loop, int counter, void *refcon)
-{
-    ImGui::XP::NewFrame();
-
-    // ImGui rendering goes here
-
-    ImGui::XP::EndFrame();
-    return -1;
-}
-
 PLUGIN_API int XPluginStart(char *out_name, char *out_signature, char *out_description)
 {
     strcpy(out_name, "X-Plane ImGui Plugin");
@@ -35,7 +25,6 @@ PLUGIN_API void XPluginDisable(void)
 
 PLUGIN_API int XPluginEnable(void)
 {
-    XPLMRegisterFlightLoopCallback(flight_loop_callback, -1, NULL);
     return 1;
 }
 
