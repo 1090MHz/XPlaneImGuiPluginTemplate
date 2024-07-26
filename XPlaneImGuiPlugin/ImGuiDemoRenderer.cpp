faf1255848d89b1b53fe3f640a8c10bb0f529e07
Alan Duffy <alan.duffy@usask.ca>
2024-07-25 20:22:31 -0600

    Use static variables to limit scope

--- XPlaneImGuiPlugin/ImGuiDemoRenderer.cpp
+++ XPlaneImGuiPlugin/ImGuiDemoRenderer.cpp
@@ -1,9 +1,9 @@
 #include "ImGuiDemoRenderer.h"
 
-// Our state
-bool show_demo_window = true;
-bool show_another_window = false;
-ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+// Our state - limit scope to this file
+static bool show_demo_window = true;
+static bool show_another_window = false;
+static ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
 void RenderDemoWindows()
 {
